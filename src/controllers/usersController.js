1e466c585486d4fd23b2abd850a6d44438cc079b
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-05-04 14:29:22 -0300

    Update usersController.js

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,4 +1,4 @@
-import { db } from "../database/database.connection.js"
+import db from "../database/database.connection.js"
 import { ObjectId } from "mongodb";
 import bcrypt from "bcrypt"
 import { v4 as uuid } from "uuid"
